c2ad7653488cab779f42218ba4a4c7c7131f6875
Harsha Vardhan Reddy Sathi <96769041+Harsha207@users.noreply.github.com>
2022-08-27 10:19:15 +0530

    import java.util.Scanner; class multiply {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a,b,multiply;         a=sc.nextInt();         b=sc.nextInt();         multiply=a*b;         System.out.println(multiply);     } }

---
+++ Multiplication.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class multiply
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,b,multiply;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        multiply=a*b;
+        System.out.println(multiply);
+    }
+}
