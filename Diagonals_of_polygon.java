6f4742c1349765fe582b5bc9ce971968a9dca6a2
Harsha Vardhan Reddy Sathi <96769041+Harsha207@users.noreply.github.com>
2022-09-09 15:11:02 +0530

    import java.util.Scanner; class Solution {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n;         n=sc.nextInt();         System.out.println(n*(n-3)/2);     } }

---
+++ Diagonals_of_polygon.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Solution
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n;
+        n=sc.nextInt();
+        System.out.println(n*(n-3)/2);
+    }
+}
