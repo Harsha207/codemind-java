994e537716a61171fc1b2dcdff18c1c7b86683ef
Harsha Vardhan Reddy Sathi <96769041+Harsha207@users.noreply.github.com>
2022-09-22 11:48:22 +0530

    import java.util.Scanner; class Solution {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],c=0;         n=sc.nextInt();         x=new int[n];         for(int i=0;i<n;i++)          x[i]=sc.nextInt();         for(int i=1;i<n-1;i++)         {             if((x[i-1]%2==0 && x[i+1]%2!=0 ) || (x[i-1]%2!=0 && x[i+1]%2==0))             {                 c+=1;             }         }         System.out.println(c);     } }

---
+++ count_numbers_between_even_and_odd_numbers_or_vice_versa.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Solution
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,x[],c=0;
+        n=sc.nextInt();
+        x=new int[n];
+        for(int i=0;i<n;i++)
+         x[i]=sc.nextInt();
+        for(int i=1;i<n-1;i++)
+        {
+            if((x[i-1]%2==0 && x[i+1]%2!=0 ) || (x[i-1]%2!=0 && x[i+1]%2==0))
+            {
+                c+=1;
+            }
+        }
+        System.out.println(c);
+    }
+}
