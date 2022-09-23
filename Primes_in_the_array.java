03693c89660bcfd6d4f37289bcf6f490f4580097
Harsha Vardhan Reddy Sathi <96769041+Harsha207@users.noreply.github.com>
2022-09-23 10:25:54 +0530

    import java.util.Scanner; class Solution {     public static boolean prime(int n)     {         int j,k=0;         for(j=1;j<=n;j++)         {             if(n%j==0)             {                 k+=1;             }         }         if(k==2)         {             return true;         }         else         {             return false;         }     }     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int x[],c=0;         x=new int[n];         for(int i=0;i<n;i++)          x[i]=sc.nextInt();         for(int i=0;i<n;i++)         {             if(prime(x[i]))             {                 c+=1;             }         }         System.out.println(c);     } }

---
+++ Primes_in_the_array.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+class Solution
+{
+    public static boolean prime(int n)
+    {
+        int j,k=0;
+        for(j=1;j<=n;j++)
+        {
+            if(n%j==0)
+            {
+                k+=1;
+            }
+        }
+        if(k==2)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int x[],c=0;
+        x=new int[n];
+        for(int i=0;i<n;i++)
+         x[i]=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            if(prime(x[i]))
+            {
+                c+=1;
+            }
+        }
+        System.out.println(c);
+    }
+}
