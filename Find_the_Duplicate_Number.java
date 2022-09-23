815f59d1b107e72c4efe357aab77aef8e9f1094c
Harsha Vardhan Reddy Sathi <96769041+Harsha207@users.noreply.github.com>
2022-09-23 11:43:34 +0530

    import java.util.Scanner; class Solution {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int x[];         x=new int[n];         for(int i=0;i<n;i++)          x[i]=sc.nextInt();         for(int i=0;i<n;i++)         {             for(int j=i+1;j<n;j++)             {                 if(x[i]==x[j])                 {                     System.out.println(x[i]);                 }             }         }     } }

---
+++ Find_the_Duplicate_Number.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class Solution
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int x[];
+        x=new int[n];
+        for(int i=0;i<n;i++)
+         x[i]=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(x[i]==x[j])
+                {
+                    System.out.println(x[i]);
+                }
+            }
+        }
+    }
+}
