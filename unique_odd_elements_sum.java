f711f04392382e5f9ad3fa4ad4c152d3aef0ad62
Harsha Vardhan Reddy Sathi <96769041+Harsha207@users.noreply.github.com>
2022-10-12 07:22:40 +0530

    import java.util.Scanner; class Solution {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],s=0;         n=sc.nextInt();         x=new int[n];         for(int i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         for(int i=0;i<n;i++)         {             for(int j=i+1;j<n;j++)             {                 if(x[i]==x[j])                 {                     x[i]=2;                 }             }             if(x[i]%2!=0 && x[i]!=2)             {                 s+=x[i];             }         }         System.out.print(s);     } }

---
+++ unique_odd_elements_sum.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class Solution
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,x[],s=0;
+        n=sc.nextInt();
+        x=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(x[i]==x[j])
+                {
+                    x[i]=2;
+                }
+            }
+            if(x[i]%2!=0 && x[i]!=2)
+            {
+                s+=x[i];
+            }
+        }
+        System.out.print(s);
+    }
+}
