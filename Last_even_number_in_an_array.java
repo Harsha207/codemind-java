f8567c93507bcb5c03e7978ce2a9035916d06539
Harsha Vardhan Reddy Sathi <96769041+Harsha207@users.noreply.github.com>
2022-10-29 10:57:53 +0530

    import java.util.Scanner; class Solution {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int x[];         x=new int[n];         for(int i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         for(int i=n-1;i>=0;i--)         {             if(x[i]%2==0)             {                 System.out.println(x[i]);                 break;             }         }     } }

---
+++ Last_even_number_in_an_array.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class Solution
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int x[];
+        x=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        for(int i=n-1;i>=0;i--)
+        {
+            if(x[i]%2==0)
+            {
+                System.out.println(x[i]);
+                break;
+            }
+        }
+    }
+}
